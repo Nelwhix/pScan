eac21ea5626c8da7c6adc96f4d7b6e6e0f14be9f
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-04-28 13:31:24 +0100

    working

--- cmd/actions_test.go
+++ cmd/actions_test.go
@@ -121,6 +121,11 @@ func TestIntegration(t *testing.T) {
 	expectedOut += fmt.Sprintf("Deleted host: %s\n", delHost)
 	expectedOut += strings.Join(hostsEnd, "\n")
 	expectedOut += fmt.Sprintln()
+	
+	for _, v := range hostsEnd {
+		expectedOut += fmt.Sprintf("%s: Host not found\n", v)
+		expectedOut += fmt.Sprintln()
+	}
 
 	if err := addAction(&out, tf, hosts); err != nil {
 		t.Fatalf("Expected no error, got %q\n", err)
@@ -138,6 +143,10 @@ func TestIntegration(t *testing.T) {
 		t.Fatalf("Expected no error, got %q\n", err)
 	}
 
+	if err := scanAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
 	if out.String() != expectedOut {
 		t.Errorf("Expected output %q, got %q\n", expectedOut, out.String())
 	}
@@ -146,7 +155,7 @@ func TestIntegration(t *testing.T) {
 func TestScanAction(t *testing.T) {
 	hosts := []string{
 		"127.0.0.1",
-		"192.168.0.1",
+		"unknownhostoutthere",
 	}
 
 	tf, cleanup := setup(t, hosts, true)
@@ -161,6 +170,36 @@ func TestScanAction(t *testing.T) {
 		}
 		defer ln.Close()
 
-		_, portStr, err := 
+		_, portStr, err := net.SplitHostPort(ln.Addr().String())
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+		ports = append(ports, port)
+		if i == 1 {
+			ln.Close()
+		}
+	}
+
+	expectedOut := fmt.Sprintln("127.0.0.1:")
+	expectedOut += fmt.Sprintf("\t%d: open\n", ports[0])
+	expectedOut += fmt.Sprintf("\t%d: closed\n", ports[1])
+	expectedOut += fmt.Sprintln()
+	expectedOut += fmt.Sprintln("unknownhostoutthere: Host not found")
+	expectedOut += fmt.Sprintln()
+
+	var out bytes.Buffer
+
+	if err := scanAction(&out, tf, ports); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
+	if out.String() != expectedOut {
+		t.Errorf("Expected output %q, got %q\n", expectedOut, out.String())
 	}
 }
--- cmd/scan.go
+++ cmd/scan.go
@@ -54,7 +54,7 @@ func printResults(out io.Writer, results []scan.Results) error {
 		message += fmt.Sprintf("%s:", r.Host)
 
 		if r.NotFound {
-			message += fmt.Sprintf("Host not found\n\n")
+			message += fmt.Sprintf(" Host not found\n\n")
 			continue
 		}
 
