7d89d134a48430d199dac302b72dd14956b70819
Nelson Isioma <nelsonisioma1@gmail.com>
2023-12-20 14:01:01 +0100

    stored hosts are now in their own dedicated folder depending on system os

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -9,20 +8,20 @@ import (
 	"io"
 	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/Nelwhix/pScan/scan"
+	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Aliases: []string{"l"},
-	Use:   "list",
-	Short: "List hosts in hosts list",
+	Use:     "list",
+	Short:   "List hosts in hosts list",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		hostsFile, err := cmd.Flags().GetString("hosts-file")
 
 		if err != nil {
-			return err 
+			return err
 		}
 
 		return listAction(os.Stdout, hostsFile, args)
@@ -37,12 +36,16 @@ func listAction(out io.Writer, hostsFile string, args []string) error {
 	hl := &scan.HostsList{}
 
 	if err := hl.Load(hostsFile); err != nil {
-		return err 
+		return err
+	}
+
+	if len(hl.Hosts) == 0 {
+		fmt.Fprintln(out, "no hosts added")
 	}
 
 	for _, h := range hl.Hosts {
 		if _, err := fmt.Fprintln(out, h); err != nil {
-			return err 
+			return err
 		}
 	}
 
--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,18 @@
 package cmd
 
 import (
+	"github.com/Nelwhix/pScan/utils"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
-
 var rootCmd = &cobra.Command{
 	Version: "0.0.2",
-	Use:   "pScan",
-	Short: "Fast TCP port scanner",
-	Long: "pScan - short for Port Scanner - executes TCP port scan on a list of hosts.\npScan allows you to add, list, and delete hosts from the list.\npScan executes a port scan on specified TCP ports. You can customize the target ports using a command line flag.",
+	Use:     "pScan",
+	Short:   "Fast TCP port scanner",
+	Long:    "pScan - short for Port Scanner - executes TCP port scan on a list of hosts.\npScan allows you to add, list, and delete hosts from the list.\npScan executes a port scan on specified TCP ports. You can customize the target ports using a command line flag.",
 }
 
 func Execute() {
@@ -22,11 +23,9 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringP("hosts-file", "f", "pScan.hosts", "pScan hosts file")
+	dataDir := utils.GetDataDir()
+	rootCmd.PersistentFlags().StringP("hosts-file", "f", filepath.Join(dataDir, "pScan.hosts"), "pScan hosts file")
 
 	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
 	rootCmd.SetVersionTemplate(versionTemplate)
 }
-
-
-
